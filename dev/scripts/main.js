e9b0abd39f765772598f60e74e2bb80e2f8d1e0e
Will Stanton <williampstanton@gmail.com>
2016-08-10 14:35:31 -0400

    added first filter function to main.js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -4,6 +4,10 @@
 var nomadApp = {};
 nomadApp.apiUrl = 'https://nomadlist.com/api/v2/list/cities';
 
+// User Input --------------------------------------------
+var userPrice = "expensive";
+
+
 // API CALL --------------------------------------------
 
 nomadApp.getCities = function() {
@@ -12,26 +16,66 @@ nomadApp.getCities = function() {
 		method: 'GET',
 		dataType: 'json'
 	}).then(function(nomadCities) {
-		nomadApp.cities = nomadCities;
+		nomadApp.cities = nomadCities.result;
+		// console.log(nomadApp.cities);
+		return nomadApp.cities;
+	})
+	.then(function(data) {
+		var filterDataByCost;
+		filterDataByCost = data.filter(function(currentCity) {
+			if (userPrice === "cheap") {
+				return currentCity.cost.nomad.USD <= 2490;
+			}
+			else if (userPrice === "affordable") {
+				return currentCity.cost.nomad.USD <= 4624 && currentCity.cost.nomad.USD >= 2491;
+			}
+			else if (userPrice === "expensive") {
+				return currentCity.cost.nomad.USD > 4624;
+			}
+		});
+		console.log(filterDataByCost);
+		return filterDataByCost;
 		
 	})
-		.fail(function(error){
-			console.log(error);
-		});	
+	.fail(function(error){
+		console.log(error);
+	});	
 };
+
+
+// .then(data => {
+// 	return filterDataByCost(data, userCostFilterParams);
+// })
+// .then(data => {
+// 	return filterDataByClimate(data, userClimateFilterParams);
+// })
+// .then(data => {
+// 	return filterDataByMonths(data, userMonthsFilterParams);
+// })
+// .then(data => {
+// 	return filterDataByActivity(data, userActivityFilterParams);
+// })
+// .then(data => {
+// 	displayData(data);
+// });
+
 
 // PRICE FILTER -----------------------------------------
 
-nomadApp.price = function(citiesResult) {
-    var userPrice = ;
-    if ()
-    citiesResult.forEach(function(city){
-    	if( city < )
-    });
 
-    citiesByPrice =//whatever we've filtered}
-	nomadApp.climate();
-}
+
+
+
+// nomadApp.price = function(citiesResult) {
+//     var userPrice = ;
+//     if ()
+//     citiesResult.forEach(function(city){
+//     	if( city < )
+//     });
+
+//     citiesByPrice =//whatever we've filtered}
+// 	nomadApp.climate();
+// }
 
 // CLIMATE FILTER ---------------------------------------
 
